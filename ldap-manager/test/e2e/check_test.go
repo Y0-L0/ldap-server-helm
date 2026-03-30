a6f219e6ba22722adcacee533398d90a49d9e550
Johannes Lohmer <jojo.dev@lohmer.com>
2026-03-30 17:17:06 +0000

    test(e2e): add RealLDAP.Check tests
    Verify root DSE search succeeds against running slapd and fails
    against an unreachable endpoint.

---
+++ ldap-manager/test/e2e/check_test.go
@@ -0,0 +1,22 @@
+package e2e
+
+import (
+	"context"
+
+	ldapadapter "github.com/y0-l0/ldap-server-helm/ldap-manager/pkg/ldap"
+)
+
+func (s *E2E) TestCheckRootDSE() {
+	err := s.backend.Check(context.Background())
+	s.Require().NoError(err)
+}
+
+func (s *E2E) TestCheckUnreachable() {
+	bad := &ldapadapter.RealLDAP{
+		URI:    "ldap://127.0.0.1:1",
+		BindDN: adminDN,
+		BindPW: adminPW,
+	}
+	err := bad.Check(context.Background())
+	s.Require().Error(err)
+}
